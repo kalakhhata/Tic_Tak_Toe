c3efcac61c044d92fea2c7a38df7fdd8cfc7b3d0
kalakhhata <ompatel2501@gmail.com>
2024-07-18 12:18:07 -0400

    I have just completed my project of makeing tic-tac-toe game. I learned lots of things like how to work with multiple functions and how I can manipulate the DOM through pure js.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 let boxes = document.querySelectorAll('.btn');
 let reset = document.querySelector('#reset');
 let turn0 = true //Player0 is Playing
+let msgC = document.querySelector('.msg-container');
+let msg = document.querySelector('#msg');
 
 const winning_pattern = [
     [0,1,2],
@@ -32,6 +34,31 @@ boxes.forEach((box)=>{
     });
 });
 
+const disabledBoxes = ()=>{
+    
+       for(let box of boxes){
+        box.disabled = true;
+       }
+    }
+
+    const enabledBoxes = ()=>{
+    
+        for(let box of boxes){
+         box.innerHTML="";   
+         box.disabled = false;
+         msgC.classList.add("hide");
+        }
+     }
+
+const showWinner = (winner)=>{
+    msg.innerHTML = `Congratulations, Winner is ${winner}`;
+    msgC.classList.remove("hide");
+    disabledBoxes();
+}
+
+reset.addEventListener('click',()=>{
+    enabledBoxes();
+})
 const checkWinner = ()=>{
    for(let pattern of winning_pattern){
     let pos1 = boxes[pattern[0]].innerHTML;
@@ -42,7 +69,7 @@ const checkWinner = ()=>{
     {
         if(pos1==pos2 && pos2==pos3)
         {
-            console.log("winner");
+            showWinner(pos1);
         }
     }
    }
